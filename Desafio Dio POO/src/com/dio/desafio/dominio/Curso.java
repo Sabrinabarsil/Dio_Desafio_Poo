4b92138b3486da2959ff83e06098c8c0da544c47
Sabrina  Barsil <barsilsabrina@gmail.com>
2022-08-10 13:34:33 -0300

    criando classe Conteudo, mentoria e curso

--- Desafio Dio POO/src/com/dio/desafio/dominio/Curso.java
+++ Desafio Dio POO/src/com/dio/desafio/dominio/Curso.java
@@ -1,38 +1,32 @@
 package com.dio.desafio.dominio;
 
-public class Curso {
+public class Curso extends Conteudo{
 
-	private String titulo;
-	private String descricao;
-	private int cargaHoraria;
+    private int cargaHoraria;
 
-	public String getTitulo() {
-		return titulo;
-	}
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public Curso() {
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
 
-	public int getCargaHoraria() {
-		return cargaHoraria;
-	}
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
 
-	public void setCargaHoraria(int cargaHoraria) {
-		this.cargaHoraria = cargaHoraria;
-	}
-
-	@Override
-	public String toString() {
-		return "Curso [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
 }
--- Desafio Dio POO/src/com/dio/desafio/dominio/Mentoria.java
+++ Desafio Dio POO/src/com/dio/desafio/dominio/Mentoria.java
@@ -2,49 +2,32 @@ package com.dio.desafio.dominio;
 
 import java.time.LocalDate;
 
-public class Mentoria {
+public class Mentoria extends Conteudo{
 
-	private String titulo;
-	private String descricao;
-	private int cargaHoraria;
-	private LocalDate data;
+    private LocalDate data;
 
-	public String getTitulo() {
-		return titulo;
-	}
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
 
-	public void setTitulo(String titulo) {
-		this.titulo = titulo;
-	}
+    public Mentoria() {
+    }
 
-	public String getDescricao() {
-		return descricao;
-	}
+    public LocalDate getData() {
+        return data;
+    }
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
 
-	public int getCargaHoraria() {
-		return cargaHoraria;
-	}
-
-	public void setCargaHoraria(int cargaHoraria) {
-		this.cargaHoraria = cargaHoraria;
-	}
-
-	public LocalDate getData() {
-		return data;
-	}
-
-	public void setData(LocalDate data) {
-		this.data = data;
-	}
-	
-	@Override
-	public String toString() {
-		return "Mentoria [titulo=" + titulo + ", descricao=" + descricao + ", cargaHoraria=" + cargaHoraria + ", data="
-				+ data + "]";
-	}
-
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
 }
